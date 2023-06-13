342d53bdb237f47be22fabe771b7aabe4f17f709
Pablo Andersen Cabello <114095552+PabloAndersen@users.noreply.github.com>
2023-06-13 18:45:32 +0200

    Delete Carrito directory

--- Carrito/php/procesarCarrito.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-$conexion = mysqli_connect("localhost", "root", "");
-            mysqli_select_db($conexion, "cartbasic1");
-            mysqli_query($conexion, "INSERT INTO carrito (`usuario`,`total`, `fechaPedido`) 
-                VALUES ('$_SESSION[usuario]', '$_SESSION[precio]', NOW())");
-print "<p>Venta procesada del usuario ".$_SESSION['usuario'].". Precio total: ".$_SESSION['precio']."</p><br>";
-print "<a href='../index.php'>Volver</a>";
-$_SESSION['precio']=null;
